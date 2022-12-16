5e2035cf06e20465ffc7d062c6704d1084c9ea8d
rohnshrma <rohn11shrma@gmail.com>
2022-12-16 18:52:32 +0530

    adding individual meal items

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
 import classes from "./AvailableMeals.module.css"
+import MealItem from "./MealItem/MealItem";
+import Card from "../UI/Card";
 
 
 const DUMMY_MEALS = [
@@ -30,10 +32,12 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = props => {
 
-    const mealList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
+    const mealList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} />)
 
     return (<section className={classes.meals}>
-        <ul>{mealList}</ul>
+        <Card>
+            <ul>{mealList}</ul>
+        </Card>
     </section>)
 
 }
