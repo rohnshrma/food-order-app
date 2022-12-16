7004ffa89547c1d3ac381ab76e0cf971e7edfeeb
rohnshrma <rohn11shrma@gmail.com>
2022-12-16 10:39:24 +0530

    header section design completed

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,6 +1,7 @@
 import React,{Fragment} from "react"
 import mealsImage from "../../assests/meals.jpg"
 import classes from "./Header.module.css"
+import HeaderCartButton from "./HeaderCartButton"
 
 
 const Header = (props) => {
@@ -8,7 +9,7 @@ const Header = (props) => {
         <Fragment>
             <header className={classes.header}>
                 <h1>Tomato</h1>
-                <button>Cart</button>
+                <HeaderCartButton/>
             </header>
             <div className={classes["main-image"]}>
                 <img src={mealsImage} alt="table full of meals" />
